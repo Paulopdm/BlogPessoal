48ee50a0ee79ffd54be18b6d5740a9928ac48470
Paulopdm <paulo.mfn@gmail.com>
2021-08-25 14:48:47 -0300

    first commit

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -0,0 +1,56 @@
+package org.generation.blogPessoal.model;
+
+import java.util.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "postagens") //vai criar uma tabela com o nome postagem;
+public class Postagem {
+	//atributos
+	@Id 
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotNull
+	@Size(min = 5, max = 100) 
+	private String titulo;
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String texto;
+	@Temporal (TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis()); //assim que passar um dado por essa classe, ele já captura a data e a hora exata.
+	
+	//gets and setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+}
