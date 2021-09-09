2fb05cb01e14cd0feac4a0ca84d11043586373ab
Paulopdm <paulo.mfn@gmail.com>
2021-09-09 17:22:42 -0300

    Implementação de testes JUnity

--- blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -8,4 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, List> {
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+	
+	public Usuario findByNome(String nome);
 }
